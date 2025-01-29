bf10b67423aaa34262873589581cd698dbf99e9a
kyibell <148383705+kyibell@users.noreply.github.com>
2025-01-29 13:11:56 -0600

    Implemented RSA Key Pair Generation and a helper function ExpiresIn to check when keys are expired

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import { generateKeyPairSync } from "crypto";
 import express from "express";
-import * as jose from 'jose';
-import jwt from "jwt";
+
 
 const app = express(); // Init app
 const port = 8080; // Variable for Port
+
+let keyIdCounter = 1; // For Kids when generating tokens so it's unique everytime
 
 function generateRSAKeyPair() { // Function to generate the RSA Key Pair
     const { publicKey, privateKey } = generateKeyPairSync('rsa', {
@@ -18,10 +19,24 @@ function generateRSAKeyPair() { // Function to generate the RSA Key Pair
             format: 'pem'
         }
     });
+    const kid = keyIdCounter++; // Increment counter for unique kids
+    const expiresIn = Math.floor(Date.now() / 1000) + 30 * 60; // 30 mins 
 
-    
+    return { // Return a key object
+        kty: 'rsa',
+        kid: kid.toString(),
+        n: publicKey,
+        e: 'AQAB',
+        exp: expiresIn
+    }
 }
 
+// isExpired functions so we only serve not expired keys
+function isExpired(ExpirationTime) {
+    const currentTime = Date.now() / 1000; 
+    
+    return currentTime > ExpirationTime; // Returns true if key is expired, else false if not
+}
 
 const server = app.listen(port, () => { // server start
     console.log(`App is listening and running on local host on port ${port}`); // Message if successful
