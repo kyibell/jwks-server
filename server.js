5621a5186e45c50b1c9929328ccfc5c26b202974
kyibell <148383705+kyibell@users.noreply.github.com>
2025-01-29 11:25:12 -0600

    Implemented first half of the RSA Key Pair generation

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+import { generateKeyPairSync } from "crypto";
 import express from "express";
+import * as jose from 'jose';
+import jwt from "jwt";
 
 const app = express(); // Init app
 const port = 8080; // Variable for Port
+
+function generateRSAKeyPair() { // Function to generate the RSA Key Pair
+    const { publicKey, privateKey } = generateKeyPairSync('rsa', {
+        modulusLength: 2048, // n parameter, determines how secure it is, typically 2048 bits
+        publicKeyEncoding: {
+            type: 'spki', //  Subject public key information (the type of key)
+            format: 'pem'
+        },
+        privateKeyEncoding: {
+            type: 'pkcs8', // Private key encoding
+            format: 'pem'
+        }
+    });
+
+    
+}
 
 
 const server = app.listen(port, () => { // server start
