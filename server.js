dc68389cb2a5765d9f421f195d17648acb2687b9
kyibell <148383705+kyibell@users.noreply.github.com>
2025-01-29 09:16:50 -0600

    Used express to start a server on port 8080

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+import express from "express";
+
+const app = express(); // Init app
+const port = 8080; // Variable for Port
+
+
+const server = app.listen(port, () => { // server start
+    console.log(`App is listening and running on local host on port ${port}`); // Message if successful
+});
+
+
+server.on('error', (error) => { // Error handling
+    console.log('Error starting server', error);
+});
 
